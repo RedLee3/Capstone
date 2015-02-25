d31668d9be66a61889487c41b3f74f3c82debbc6
RedLee3 <leeepling92@gmail.com>
2015-02-24 19:52:49 -0500

    Updating everything

--- 2parse2db.rb
+++ 2parse2db.rb
@@ -3,16 +3,16 @@ require 'sqlite3'
  
 db = SQLite3::Database.new "test.db"
 
-db.execute "CREATE TABLE IF NOT EXISTS hosts_table( ip TEXT, mac TEXT, name TEXT )"
+db.execute ("CREATE TABLE IF NOT EXISTS hosts_table( ip TEXT, mac TEXT, name TEXT )")
 
 Nmap::XML.new(open ARGV[0]) do |xml|
   xml.each_host do |host|
     ip = host.ip
-    puts ip
     mac = host.mac
-    puts mac
     name = host.hostnames[0]
-    puts name
+    #puts ip
+    #puts mac
+    #puts name
     #host.os.matches[0]
     #host.os.matches[1]
     #host.os.matches[2]
