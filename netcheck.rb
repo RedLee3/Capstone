5775ebf53d7a217bc6f3b36a812918adf3c51f9c
root <root@localhost.localdomain>
2015-03-22 19:54:09 -0400

    adding comments

--- netcheck.rb
+++ netcheck.rb
@@ -1,18 +1,20 @@
 #!/usr/bin/ruby
 
+#Command and stuff to get the ip address from ifconfig
 command = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 
 i = 1
 
 while command == ""
   command = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-  puts "no ip on try ##{i}"
+  puts "no ip on try ##{i}"#For counting failed attempts, could be removed. 
   i += 1
   sleep 2
 end
 
 ip = command
-mac = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f4`
+mac = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f4` #Command to get MAC address
 
+#Putting variables. Can be pointed elsewhere as needed.
 puts ip.chomp
 puts mac.chomp
