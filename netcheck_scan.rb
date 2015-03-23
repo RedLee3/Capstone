693426d9afdcdebf3fd9b4c19f81fb81251b9355
Lee <RedLee3@users.noreply.github.com>
2015-03-22 20:58:34 -0400

    Create netcheck_scan.rb
    A script to get ip/mask and then scan from msfcli

---
+++ netcheck_scan.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+#Command and stuff to get the ip address from ifconfig
+command = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+
+i = 1
+
+while command == ""
+  command = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+  puts "no ip on try ##{i}"#For counting failed attempts, could be removed. 
+  i += 1
+  sleep 2
+end
+
+ip = command
+mac = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f4` #Command to get MAC address
+
+#Doing a sc
+scan = `msfcli auxiliary/scanner/portscan/tcp rhosts=127.0.0.1 E`
