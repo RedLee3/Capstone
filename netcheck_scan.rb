16def6e9e982e970cb7f4e06f9a7372f4ef78453
Lee <RedLee3@users.noreply.github.com>
2015-03-23 21:49:26 -0400

    Update netcheck_scan.rb
    dding cidr ability

--- netcheck_scan.rb
+++ netcheck_scan.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'ipaddr'
+
 #Command and stuff to get the ip address from ifconfig
 command = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 
@@ -15,5 +17,8 @@ end
 ip = command
 mac = `ifconfig eth0 | grep 'inet addr:' | cut -d: -f4` #Command to get MAC address
 
+cidr = IPAddr.new(mac).to_i.to_s(2).count("1")
+
 #Doing a sc
-scan = `msfcli auxiliary/scanner/portscan/tcp rhosts=127.0.0.1 E`
+#scan = `msfcli auxiliary/scanner/portscan/tcp rhosts=127.0.0.1 E'
+scan = `msfconsole -x "db_scan #{ip}/#{cidr} -O"`
